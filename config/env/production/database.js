c28a08c7d4655904a38f02c3a5c64d2c234db06c
An Xuan Hoang <xuanan2001@gmail.com>
2021-07-07 01:05:36 +0700

    Update for self hosted option

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,13 +1,13 @@
-const parse = require('pg-connection-string').parse;
+const parse = require("pg-connection-string").parse;
 const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
+        client: "postgres",
         host: config.host,
         port: config.port,
         database: config.database,
@@ -18,7 +18,7 @@ module.exports = ({ env }) => ({
         },
       },
       options: {
-        ssl: true,
+        ssl: false,
       },
     },
   },
